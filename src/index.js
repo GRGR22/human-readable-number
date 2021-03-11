2724af9674a0a62ecda3a4d7e999055b9fec3e11
GR22 <bagbox@mail.ru>
2021-03-11 09:57:16 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    let mainStr = '';
+    let z = 0
+    let small = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen","sixteen","seventeen", "eighteen", "nineteen"];
+    let big = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    if (number == 0) return 'zero'
+      if (number >= 100) {
+        z = Math.floor(number/100);
+        number-=z*100;
+        mainStr+=small[z-1] + " hundred"
+        if (number > 0) mainStr+=" "
+      }
+      
+      if (number < 100 && number >= 20) {
+        z = Math.floor(number/10);
+        number-=z*10;
+        mainStr+=big[z-2]
+        if (number > 0) mainStr+=" "
+      }
+      
+      if (number < 20 && number > 0) mainStr +=small[number-1];
+
+  return mainStr
 }
